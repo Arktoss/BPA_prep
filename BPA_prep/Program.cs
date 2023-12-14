1410d63a28b1995913d91901df0125bcfbd32cb1
Arktoss <111780704+Arktoss@users.noreply.github.com>
2023-12-14 13:02:39 -0500

    Add project files.

---
+++ BPA_prep/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingAndWritingToCSV
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string sourceFilePath = "C:\\Users\\User\\source\\repos\\ReadingAndWritingToCSV\\ReadingAndWritingToCSV\\Sample_Data_Age.csv";
+            string destinationFilePath = "C:\\Users\\User\\source\\repos\\ReadingAndWritingToCSV\\ReadingAndWritingToCSV\\Numbers.csv";
+
+            ProcessCsvFile(sourceFilePath, destinationFilePath);
+        }
+
+        private static void ProcessCsvFile(string sourceFilePath, string destinationFilePath)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                foreach (string line in lines)
+                {
+                    string[] values = line.Split(',');
+
+                    foreach (string value in values)
+                    {
+                        if (double.TryParse(value, out double numericValue) && numericValue > 65)
+                        {
+                            File.AppendAllText(destinationFilePath, line + Environment.NewLine);
+                            break; // No need to continue processing this line once a match is found
+                        }
+                    }
+                }
+
+                Console.WriteLine("Processing completed successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
